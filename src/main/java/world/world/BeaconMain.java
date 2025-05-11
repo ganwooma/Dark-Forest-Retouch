02d8479b35fcf5da53c8f4ba78e8d186413a0f9d
Eclipse <mandi8976@gmail.com>
2025-05-11 17:06:45 +0900

    Update BeaconMain.java

--- src/main/java/world/world/BeaconMain.java
+++ src/main/java/world/world/BeaconMain.java
@@ -62,7 +62,7 @@ public class BeaconMain extends JavaPlugin implements Listener {
                 event.setQuitMessage(null); // 퇴장 메시지 숨기기
             }
         }, this);
-        
+
         familyManager = new FamilyManager(this);
         banManager = new BanManager();
         reviveManager = new PlayerReviveManager(this);
